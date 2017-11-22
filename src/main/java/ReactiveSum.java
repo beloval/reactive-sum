c32275b6ee5348558b69766e7392d8d60f0049c5
Belov Aleksandr <aleksandr.belov@pj.ca>
2017-11-22 09:11:23 -0500

    reactive sum without lambdas

---
+++ src/main/java/ReactiveSum.java
@@ -0,0 +1,33 @@
+import rx.Observable;
+import rx.Observer;
+import rx.functions.Func2;
+
+public final class ReactiveSum implements Observer<Double> { //1
+
+    private double sum;
+    public ReactiveSum(Observable<Double> a, Observable<Double> b) {
+        this.sum = 0;
+        Observable.combineLatest(a, b, new Func2<Double, Double, Double>() { // (5)
+            public Double call(Double a, Double b) {
+                return a + b;
+            }
+        }).subscribe(this); // (6)
+    }
+
+    public void onCompleted() {
+        System.out.println("Exiting last sum was : " + this.sum); //(4)
+    }
+
+
+    public void onError(Throwable e) {
+        System.err.println("Got an error!"); // (3)
+        e.printStackTrace();
+    }
+
+    public void onNext(Double sum) {
+        this.sum = sum;
+        System.out.println("update : a + b = " + sum); // (2)
+    }
+}
+
+
